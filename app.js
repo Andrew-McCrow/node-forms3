149f1fb10785002ed1de7ac047274823a40fcbb8
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-23 12:17:10 +0000

    added logic for editing users & added form validation logic

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const app = express();
 const path = require("node:path");
 const usersRouter = require("./routes/usersRouter");
 
+// middleware for parsing form data and setting up our view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+// routing all requests to the usersRouter
 app.use("/", usersRouter);
 
+// listening for requests on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
